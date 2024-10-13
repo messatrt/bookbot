8b3c0187ab3f28dce1d142b8e32ce3cce2765c29
Your Name <yourname@example.com>
2024-10-13 09:04:13 +0530

    added the ability to read files

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main():
+    path = "./books/frankenstien.txt"
+    book = get_book(path)
+    print(book)
+
+def get_book(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
